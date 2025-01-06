f94aecf7eca1a40ef771a16b6bbde25df48e49b6
Dhani5703 <dems9811@naver.com>
2025-01-06 18:30:47 +0900

    feat: 달력 컴포넌트 확장 가능하도록 props 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,27 @@
 // pages/index.tsx
 import React, { useState } from 'react';
-import CalendarTimePicker from '../components/@shared/CalendarPicker';
+import CalendarPicker from '../components/@shared/CalendarPicker';
 
 export default function Home() {
-  const [selectedDatetime, setSelectedDatetime] = useState<Date>(new Date());
+  const [selectedDate, setSelectedDate] = useState<Date>(new Date());
 
   const handleDateChange = (date: Date) => {
     console.log('선택한 날짜+시간:', date);
-    setSelectedDatetime(date);
+    setSelectedDate(date);
   };
 
   return (
     <div className="p-4">
       <h1 className="text-xl font-bold mb-4">모임 찾기 필터</h1>
-      <CalendarTimePicker
+      <CalendarPicker
         onChange={handleDateChange}
-        //initialDate={new Date()}
+        //minDate={new Date()}
         // UI 스타일을 Tailwind로 더 꾸밀 수 있음
         //containerClassName="flex flex-col md:flex-row bg-white p-4 rounded-xl shadow space-y-4 md:space-y-0 md:space-x-6"
         //timeItemSelectedClassName="bg-orange-500 text-white"
       />
       <div className="mt-6">
-        <p>현재 선택된 날짜+시간: {selectedDatetime.toLocaleString()}</p>
+        <p>현재 선택된 날짜: {selectedDate.toLocaleDateString()}</p>
       </div>
     </div>
   );
