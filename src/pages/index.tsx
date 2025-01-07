f55a61acdf4f3b6143336b660868a8bb2d2ff9ba
kimsayhi <kimse418@gmail.com>
2025-01-07 14:55:45 +0900

    remove: test용 데이터 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,3 @@
-'use client';
-import useModal from '@/hooks/useModal';
-
 export default function Home() {
-  const { Modal, handleOpenModal } = useModal();
-  return (
-    <div className="h-[2000px]">
-      <button onClick={handleOpenModal}>모달열기</button>
-      <Modal title="모딜 예시">hello, Modal!</Modal>
-    </div>
-  );
+  return;
 }
