cdd9ea250863c5604a623fbe1ebe7a89bff6bf34
yunsungLEE <chance07236@naver.com>
2025-01-09 16:09:08 +0900

    chore: 테일윈드 prettier 설치

--- src/components/shared/input/index.tsx
+++ src/components/shared/input/index.tsx
@@ -9,7 +9,7 @@ function Input(props: InputProps) {
   const { type = 'input', placeholder = '입력해 주세요.' } = props;
   return (
     <input
-      className="h-mobileInputHeight md:h-tabletInputHeight"
+      className="h-mobileInputHeight p-4 pt-2 md:h-tabletInputHeight"
       type={type}
       placeholder={placeholder}
       {...props}
