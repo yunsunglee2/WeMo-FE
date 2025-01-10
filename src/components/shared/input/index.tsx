62e77ea53b185b81df42f916e7b68d07989b4a64
yunsungLEE <chance07236@naver.com>
2025-01-10 15:24:39 +0900

    fix: input 공통 컴포넌트 className 추가, 프롭스 오버라이딩 문제 해결을 위한 rest 연산자 추가 및 twMerge 적용

--- src/components/shared/input/index.tsx
+++ src/components/shared/input/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { twMerge } from 'tailwind-merge';
 import { InputType } from './type';
 
 export interface InputProps
@@ -6,17 +7,26 @@ export interface InputProps
   type?: InputType;
   id: string;
   ariaLabel?: string;
+  inputClassName: string;
 }
 
 // props 추가 작성 가능
 // className 추가 작성 가능
 // 피드백 환영입니다! 공통 컴포넌트 만드는 거 어렵네요,,,
 function Input(props: InputProps) {
-  const { type = 'text', placeholder = '입력해 주세요.', ariaLabel } = props;
+  const {
+    type = 'text',
+    placeholder = '입력해 주세요.',
+    ariaLabel,
+    inputClassName,
+  } = props;
+  const className = twMerge(
+    `h-mobileInputHeight shrink-0 flex-grow rounded-xl bg-gray-50 p-3 pt-2 text-base placeholder-gray-500 md:h-tabletInputHeight ${inputClassName}`,
+  );
   return (
     <input
       aria-label={ariaLabel}
-      className="h-mobileInputHeight shrink-0 flex-grow rounded-xl bg-gray-50 p-3 pt-2 text-base placeholder-gray-500 md:h-tabletInputHeight"
+      className={className}
       type={type}
       placeholder={placeholder}
       {...props}
