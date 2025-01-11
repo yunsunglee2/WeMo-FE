2165b4d4cd7b6bee052140e01553d005b227d7b8
kimsayhi <kimse418@gmail.com>
2025-01-11 23:37:19 +0900

    feat: 모임, 일정에 대한 response타입 저장장

---
+++ src/components/types/api/plan.ts
@@ -0,0 +1,43 @@
+interface User {
+  nickname: string;
+  profileImagePath: string;
+}
+
+interface MeetingInfo {
+  meetingId: number;
+  meetingName: string;
+  description: string;
+  meetingImagePath: string;
+}
+
+interface PlanData {
+  planId: number;
+  nickname: string;
+  profileImagePath: string;
+  planName: string;
+  category: string;
+  address: string;
+  longitude: number;
+  latitude: number;
+  planImagePath: string;
+  content: string;
+  dateTime: string;
+  registrationEnd: string;
+  capacity: number;
+  participants: number;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string | null;
+  userList: User[];
+  meetingInfo: MeetingInfo[];
+  isCanceled: boolean;
+  isLiked: boolean;
+  isOpened: boolean;
+  isFulled: boolean;
+}
+
+export interface GET_PLAN_DETAIL_RESPONSE {
+  success: boolean;
+  message: string;
+  data: PlanData;
+}
