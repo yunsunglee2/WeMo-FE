283482dc843ad04354f3a9a77601ff5f50e0de6e
yunsungLEE <chance07236@naver.com>
2025-01-13 17:01:54 +0900

    design: 공통 버튼 컴포넌트 회원가입 버튼 색상 수정

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,3 +1,37 @@
+import Input from '@/components/shared/input';
+import withLabel from '@/components/shared/input/HoC/withLabel';
+import Button from '@/components/shared/Button';
+
 export default function Signup() {
-  return <div>Signup</div>;
+  const InputWithLabel = withLabel(Input);
+  return (
+    <form action="">
+      <InputWithLabel
+        id={'name'}
+        placeholder={'이름을 입력해 주세요.'}
+        name={'이름'}
+      />
+      <InputWithLabel
+        id={'company-name'}
+        placeholder={'회사명을 입력해 주세요.'}
+        name={'회사명'}
+      />
+      <InputWithLabel
+        id={'email'}
+        placeholder={'이메일 주소를 입력해 주세요.'}
+        name={'이메일 주소'}
+      />
+      <InputWithLabel
+        id={'password'}
+        placeholder={'비밀번호를 입력해 주세요.'}
+        name={'이메일 주소'}
+      />
+      <Input
+        id={'password-verifing'}
+        aria-label={'password-verifing'}
+        placeholder={'비밀번호를 다시 입력해 주세요.'}
+      />
+      <Button text={'회원가입'} type="signup" />
+    </form>
+  );
 }
