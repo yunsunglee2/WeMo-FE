f6bd19b5cd49284c561d8e61f191fe341c267e66
김민규 <mingyu@MinQs-MacBook-Pro.local>
2025-01-14 15:28:59 +0900

    feat: 드롭다운 전체 관리하는 filterBar 생성

--- src/components/shared/HeartRating.tsx
+++ src/components/shared/HeartRating.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 interface HeartRatingProps {
   rating: number; // 현재 평점
-  maxRating?: number; // 최대 평점 (기본값: 5)
+  maxRating?: number; // 최대 평점
   onRate?: (newRating: number) => void; // 평점 변경 핸들러
 }
 
@@ -18,7 +18,7 @@ const HeartRating: React.FC<HeartRatingProps> = ({
   };
 
   return (
-    <div className="flex gap-2 justify-center">
+    <div className="flex justify-center gap-2">
       {Array.from({ length: maxRating }, (_, index) => (
         <img
           key={index}
@@ -28,12 +28,7 @@ const HeartRating: React.FC<HeartRatingProps> = ({
               : '/assets/icons/emptyHeart.svg'
           }
           alt={`Heart ${index + 1}`}
-          className="
-            cursor-pointer
-            w-6 h-6            
-            md:w-8 md:h-8    // 태블릿
-            xl:w-10 xl:h-10    // PC
-          "
+          className="// 태블릿 // PC h-6 w-6 cursor-pointer md:h-8 md:w-8 xl:h-9 xl:w-9"
           onClick={() => handleClick(index)}
         />
       ))}
