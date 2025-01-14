950245c8bb2ebe23a57abdf99c26b5caac14e4b2
yunsungLEE <chance07236@naver.com>
2025-01-14 09:44:15 +0900

    design: 공통 버튼 컴포넌트 start-signup 버튼 css 추가 및 #fff 색상 코드 -> primary-100 으로 수정

--- src/pages/start/index.tsx
+++ src/pages/start/index.tsx
@@ -5,9 +5,7 @@ const StartPage = () => {
   return (
     <div className="flex h-screen flex-col items-center justify-center bg-white">
       <Button text={'비회원으로 시작하기'} type={'start'} />
-      <button className="mb-6 h-12 w-72 rounded-lg bg-teal-500 font-bold text-white">
-        회원 가입
-      </button>
+      <Button text={'회원가입'} type={'start-signup'} />
       <p className="mb-4 font-medium text-gray-600">
         이미 가입하셨나요? <span className="underline">로그인하기</span>
       </p>
