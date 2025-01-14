f8b84424cb6dd19a29caa23717e1918ee19bd000
kimsayhi <kimse418@gmail.com>
2025-01-14 11:41:49 +0900

    feat: 날짜 뱃지, 아바타, 아바타 리스트 컴포넌트 구현, 프로그래스바 색상 수정

--- src/components/shared/ProgressBar.tsx
+++ src/components/shared/ProgressBar.tsx
@@ -11,21 +11,13 @@ export default function ProgressBar({
 }: ProgressBarProps) {
   const progress = (participants / capacity) * 100;
   return (
-    <div className="relative h-1 w-full rounded-full bg-gray-300">
+    <div className="relative h-1 w-full rounded-full bg-primary-95">
       <motion.div
         initial={{ width: '0%' }}
         animate={{ width: `${progress}%` }}
         transition={{ ease: 'easeOut', duration: 0.7 }}
-        className="absolute left-0 top-0 h-1 rounded-full bg-orange-500"
-      >
-        {/* <div className="absolute -right-[30px] top-[17px]">
-          <div className="relative max-w-xs rounded-lg bg-orange-500 px-2 py-[2px] text-sm text-white">
-            <span>{`${participants} 명`}</span>
-            <p className="font-sans text-base font-semibold"></p>
-            <div className="absolute bottom-5 right-[26px] h-0 w-0 border-b-[15px] border-l-[5px] border-r-[5px] border-transparent border-b-orange-500"></div>
-          </div>
-        </div> */}
-      </motion.div>
+        className="absolute left-0 top-0 h-1 rounded-full bg-primary-10"
+      ></motion.div>
     </div>
   );
 }
