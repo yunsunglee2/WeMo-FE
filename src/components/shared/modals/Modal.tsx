7fb2c75facd7c9e6121a4a1dd96e77532db0d181
kimsayhi <kimse418@gmail.com>
2025-01-15 11:49:48 +0900

    fix: 모달 디자인 수정

--- src/components/shared/modals/Modal.tsx
+++ src/components/shared/modals/Modal.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren } from 'react';
 import ModalPortal from './ModalPortal';
 import ModalBackDrop from './ModalBackDrop';
+import Image from 'next/image';
 
 interface ModalProps {
   handleClose: () => void;
@@ -22,8 +23,17 @@ export default function Modal({
         <div className="fixed left-1/2 top-1/2 z-[11] flex w-full max-w-screen-md -translate-x-1/2 -translate-y-1/2 p-6">
           <div className="flex w-full flex-col rounded-lg bg-white p-6 text-black">
             <div className="flex justify-end pb-10">
-              {title ? <span className="grow">{title}</span> : null}
-              <button onClick={handleClose}>닫기버튼</button>
+              {title ? (
+                <span className="grow text-xl font-semibold">{title}</span>
+              ) : null}
+              <button onClick={handleClose}>
+                <Image
+                  src="/assets/icons/x.svg"
+                  alt="닫기버튼"
+                  width={30}
+                  height={30}
+                />
+              </button>
             </div>
             <div className="h-full w-full">{children}</div>
           </div>
