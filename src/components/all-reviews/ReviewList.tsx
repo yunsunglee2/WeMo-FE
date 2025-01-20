7fabfdf52f3938a23eb9656f776ab7d42c49b73f
김민규 <mingyu@MinQs-MacBook-Pro.local>
2025-01-21 02:59:05 +0900

    refactor: 리뷰 이미지 코드 수정

--- src/components/all-reviews/ReviewList.tsx
+++ src/components/all-reviews/ReviewList.tsx
@@ -24,11 +24,11 @@ const ReviewList: React.FC<ReviewListProps> = ({ reviews }) => {
           className="flex flex-col items-start rounded-lg bg-white p-4 shadow-md md:flex-row md:items-center"
         >
           <img
-            src={review.planImagePath}
+            src={review.reviewImagePath}
             alt={review.planName}
-            className="mb-4 h-32 w-full rounded-md object-cover md:mb-0 md:mr-4 md:h-24 md:w-24"
+            className="mb-4 aspect-[5/3] w-full rounded-md object-contain md:mb-0 md:mr-4 md:h-24 md:w-24"
           />
-          <div className="flex-1">
+          <div className="w-full flex-1">
             <div className="flex flex-col md:flex-row md:justify-between">
               <h2 className="text-base font-semibold text-gray-800">
                 {review.planName}
