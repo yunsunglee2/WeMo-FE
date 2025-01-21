f5d12d0d01d208db6d9cd70bc751f3c28a399c5c
yunsungLEE <chance07236@naver.com>
2025-01-21 18:03:05 +0900

    chore: 아이콘 추가

--- src/constants/gnbMenu.ts
+++ src/constants/gnbMenu.ts
@@ -1,5 +1,9 @@
 export const menuItems = [
-  { name: '홈', path: '/meetings' },
-  { name: '모임 찾기', path: '/search-meetings' },
-  { name: '모든 리뷰', path: '/all-reviews' },
+  { name: '홈', path: '/meetings', icon: '/assets/icons/GNB_home' },
+  {
+    name: '모임 찾기',
+    path: '/search-meetings',
+    icon: '/assets/icons/GNB_home',
+  },
+  { name: '모든 리뷰', path: '/all-reviews', icon: '/assets/icons/GNB_review' },
 ];
