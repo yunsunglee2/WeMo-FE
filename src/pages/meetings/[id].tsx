e5d64a83e2ba4c4c2c975eb8d77de731d459f6bc
kimsayhi <kimse418@gmail.com>
2025-01-23 11:33:10 +0900

    refactor : useQuery 커스텀훅 타입 안정성 강화

--- src/pages/meetings/[id].tsx
+++ src/pages/meetings/[id].tsx
@@ -2,9 +2,14 @@ import MeetingDetailFooter from '@/components/meetings/MeetingDetailFooter';
 import MeetingDetailMain from '@/components/meetings/MeetingDetailMain';
 import Header from '@/components/shared/layout/Header';
 import useMeetingDetailQuery from '@/hooks/useMeetingDetailQuery';
+import { useRouter } from 'next/router';
 
 export default function MeetingDetailPage() {
-  const { isLoading, data, isError } = useMeetingDetailQuery();
+  const router = useRouter();
+  const { id } = router.query;
+  const { isLoading, data, isError } = useMeetingDetailQuery(
+    typeof id === 'string' ? parseInt(id) : null,
+  );
 
   if (isLoading) return <div>로딩중</div>;
   if (!data || isError) return <div>데이터 불러오기 실패</div>;
