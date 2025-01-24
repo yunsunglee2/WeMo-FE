ddd57f9f9f5b2c59c9b0e10677ae4d697929ae66
G1Coding <32190250@dankook.ac.kr>
2025-01-24 17:53:00 +0900

    refactor: 마이페이지 meeting, plan 페이지 타입, 데이터 호출 분리

--- src/pages/user/[username]/meeting.tsx
+++ src/pages/user/[username]/meeting.tsx
@@ -1,74 +1,32 @@
 import MeetingCard from '@/components/mypage/MeetingCard';
-// import Button from '@/components/shared/Button';
-import { useEffect, useState } from 'react';
-import { StaticImageData } from 'next/image';
-import axios from 'axios';
+import { useState } from 'react';
 import NoData from '@/components/mypage/NoData';
 import MypageLayout from '@/components/mypage/MypageLayout';
-import { useRouter } from 'next/router';
-
-// const BASE_URL = process.env.NEXT_PUBLIC_API_KEY;
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-
-export interface MeetingData {
-  email: string;
-  meetingId: number;
-  meetingName: string;
-  meetingImagePath: StaticImageData;
-  category: string;
-  memberCount: number;
-}
+import { MeetingData } from '@/types/mypageType';
+import useFetchData from '@/api/mypage/useFetchData';
 
 export default function MyMeeting() {
   const [activeTab, setActiveTab] = useState<'tabLeft' | 'tabRight'>('tabLeft');
-  const [meetings, setMeetings] = useState<MeetingData[]>([]);
   //유저 정보 전역 데이터로 수정하기@@@
   const useremail = 'test123@test.com'; // 현재 사용자의 이메일
-  const router = useRouter();
+  const {
+    data: meetings,
+    loading,
+    error,
+  } = useFetchData<MeetingData[]>('/api/users/meetings?page=1', 'meetingList');
 
-  //최초 렌더링 시에만 api 호출
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          `${BASE_URL}/api/users/meetings?page=1`,
-          { withCredentials: true },
-        );
-        const userMeetingData = response.data.data.meetingList;
-        const userMeetingCount = response.data.data.meetingCount;
+  // console.log('업데이트 데이터', meetings);
 
-        console.log('들어온 데이터 수', userMeetingCount);
-
-        setMeetings(userMeetingData);
-      } catch (error: unknown) {
-        if (axios.isAxiosError(error) && error.response) {
-          console.log('서버로부터 받은 에러 데이터', error.response.data);
-          if (error.response.status === 400) {
-            alert('로그인이 필요합니다!.');
-            router.push('/login');
-            return;
-          } else {
-            alert('[error] 서버와 통신 오류 발생.');
-          }
-        } else {
-          //axios 에러가 아닌 다른 예외가 발생한 경우
-          alert('[error] 오류가 발생했습니다. 다시 시도해주세요.');
-        }
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  console.log('업데이트 데이터', meetings);
-
-  const createdMeetings = meetings.filter((meet) => meet.email === useremail);
-  const joinedMeetings = meetings.filter((meet) => meet.email !== useremail);
-  console.log('내가 만든 모임데이터', createdMeetings);
-  console.log('참여한 데이터', joinedMeetings);
+  const createdMeetings = meetings?.filter((meet) => meet.email === useremail);
+  const joinedMeetings = meetings?.filter((meet) => meet.email !== useremail);
+  // console.log('내가 만든 모임데이터', createdMeetings);
+  // console.log('참여한 데이터', joinedMeetings);
 
   const meetingData =
     activeTab === 'tabLeft' ? joinedMeetings : createdMeetings;
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
 
   return (
     <MypageLayout
@@ -82,16 +40,12 @@ export default function MyMeeting() {
     >
       <section className="mt-5 flex flex-col items-center sm:w-[350px] md:w-[700px] lg:w-[1050px]">
         <ul className="flex flex-col gap-y-10 md:flex-row md:flex-wrap md:gap-10">
-          {meetingData.length > 0 ? (
+          {meetingData && meetingData.length > 0 ? (
             meetingData.map((meet, index) => (
-              <MeetingCard
-                key={index}
-                meetingData={meet}
-                useremail={useremail}
-              />
+              <MeetingCard key={index} meetingData={meet} />
             ))
           ) : (
-            <NoData comment="모임이" toPage="모임 둘러보기" />
+            <NoData comment="모임이" toPage="/user/1" text="모임 구경하기" />
           )}
         </ul>
       </section>
--- src/pages/user/[username]/plan.tsx
+++ src/pages/user/[username]/plan.tsx
@@ -1,83 +1,31 @@
-import { useEffect, useState } from 'react';
-import { StaticImageData } from 'next/image';
-import axios from 'axios';
+import { useState } from 'react';
 import PlanCard from '@/components/mypage/PlanCard';
 import NoData from '@/components/mypage/NoData';
 import MypageLayout from '@/components/mypage/MypageLayout';
-import { useRouter } from 'next/router';
-
-// const BASE_URL = process.env.NEXT_PUBLIC_API_KEY;
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-
-export interface PlanData {
-  email: string;
-  planId: number;
-  planName: string;
-  category: string;
-  province: string;
-  district: string;
-  planImagePath: StaticImageData | string;
-  dateTime: string;
-  meetingId: number;
-  meetingName: string;
-  capacity: number;
-  participants: number;
-  registrationEnd: string; // 마감 날짜
-  isOpened: boolean;
-  isCancled: boolean;
-  isFulled: boolean;
-  isLiked: boolean;
-}
+import { PlanData } from '@/types/mypageType';
+import useFetchData from '@/api/mypage/useFetchData';
 
 export default function MyMeMyPlaneting() {
   const [activeTab, setActiveTab] = useState<'tabLeft' | 'tabRight'>('tabLeft');
-  const [plans, setPlans] = useState<PlanData[]>([]);
   //유저 정보 전역 데이터로 수정하기@@@
   const useremail = 'test123@test.com'; // 현재 사용자의 이메일
-  const router = useRouter();
 
-  //최초 렌더링 시에만 api 호출
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(`${BASE_URL}/api/users/plans?page=1`, {
-          withCredentials: true,
-        });
-        const userPlanData = response.data.data.planList;
-        const userPlanCount = response.data.data.planCount;
-
-        console.log('들어온 데이터 수', userPlanCount);
-
-        setPlans(userPlanData);
-      } catch (error: unknown) {
-        if (axios.isAxiosError(error) && error.response) {
-          console.log('서버로부터 받은 에러 데이터', error.response.data);
-          if (error.response.status === 400) {
-            alert('로그인이 필요합니다!.');
-            router.push('/login');
-            return;
-          } else {
-            alert('[error] 서버와 통신 오류 발생.');
-          }
-        } else {
-          //axios 에러가 아닌 다른 예외가 발생한 경우
-          alert('[error] 오류가 발생했습니다. 다시 시도해주세요.');
-        }
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  console.log('일정 업데이트 데이터', plans);
+  const {
+    data: plans,
+    loading,
+    error,
+  } = useFetchData<PlanData[]>('/api/users/plans?page=1', 'planList');
 
   // 나중에 이메일로 바꾸기 ==-======================
-  const createdPlans = plans.filter((plan) => plan.email === useremail);
-  const joinedPlans = plans.filter((plan) => plan.email !== useremail);
+  const createdPlans = plans?.filter((plan) => plan.email === useremail);
+  const joinedPlans = plans?.filter((plan) => plan.email !== useremail);
   console.log('내가 만든 일정데이터', createdPlans);
   console.log('참여한 일정데이터', joinedPlans);
 
   const planData = activeTab === 'tabLeft' ? joinedPlans : createdPlans;
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
 
   return (
     <MypageLayout
@@ -91,13 +39,13 @@ export default function MyMeMyPlaneting() {
     >
       <section className="mt-4 flex flex-col sm:w-[500px] md:w-[650px] lg:w-[850px]">
         <ul className="flex flex-col gap-y-10">
-          {planData.length > 0 ? (
+          {planData && planData.length > 0 ? (
             planData.map((plan, index) => (
-              <PlanCard key={index} planData={plan} useremail={useremail} />
+              <PlanCard key={index} planData={plan} />
             ))
           ) : (
             // 배열에 데이터가 없는 경우
-            <NoData comment="일정이" toPage="일정 둘러보기" />
+            <NoData comment="일정이" toPage="/" text="일정 보러가기" />
           )}
         </ul>
       </section>
