fdc8f95f9573cb67c5a2a8ba16a763d9cb90e406
kimsayhi <kimse418@gmail.com>
2025-01-24 16:10:12 +0900

    feat: 모임 주최자만 일정생성하기 버튼 보임

--- src/components/meetings/MeetingDetailFooter.tsx
+++ src/components/meetings/MeetingDetailFooter.tsx
@@ -1,11 +1,14 @@
 import EditPlanButton from './editPlan/EditPlanButton';
-
-export default function MeetingDetailFooter() {
-  const admin = true;
+interface MeetingDetailFooterProps {
+  isHost: boolean;
+}
+export default function MeetingDetailFooter({
+  isHost,
+}: MeetingDetailFooterProps) {
   return (
     <>
       <div className="flex-center h-[60px] w-full border-t bg-white px-3 md:border-t-0">
-        {admin ? (
+        {isHost ? (
           <EditPlanButton />
         ) : (
           <button className="flex-center z-[9] h-[42px] w-full rounded-md bg-primary-10 font-bold text-white">
--- src/pages/meetings/[id].tsx
+++ src/pages/meetings/[id].tsx
@@ -2,15 +2,25 @@ import MeetingDetailFooter from '@/components/meetings/MeetingDetailFooter';
 import MeetingDetailMain from '@/components/meetings/MeetingDetailMain';
 import Header from '@/components/shared/layout/Header';
 import useMeetingDetailQuery from '@/hooks/useMeetingDetailQuery';
+import { RootState } from '@/redux/store';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 export default function MeetingDetailPage() {
+  const [isHost, setIsHost] = useState(false);
   const router = useRouter();
   const { id } = router.query;
   const { isLoading, data, isError } = useMeetingDetailQuery(
     typeof id === 'string' ? parseInt(id) : null,
   );
+  const user = useSelector((state: RootState) => state.auth.user);
 
+  useEffect(() => {
+    if (user?.email === data?.data.email) {
+      setIsHost(true);
+    }
+  }, [data?.data.email, user?.email]);
   if (isLoading) return <div>로딩중</div>;
   if (!data || isError) return <div>데이터 불러오기 실패</div>;
   return (
@@ -18,7 +28,7 @@ export default function MeetingDetailPage() {
       <Header title="모임 상세" />
       <div className="mx-auto max-w-screen-md">
         <MeetingDetailMain meetingData={data.data} />
-        <MeetingDetailFooter />
+        <MeetingDetailFooter isHost={isHost} />
       </div>
     </>
   );
