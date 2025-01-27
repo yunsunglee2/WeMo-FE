b58e81cf9791cc5ac6d7760d5dfc1f70a2cbcf54
yunsungLEE <chance07236@naver.com>
2025-01-27 17:42:06 +0900

    feat: google oAuth api 기능 구현

--- src/components/auth/login/oAuth/button/buttonGoogle.tsx
+++ src/components/auth/login/oAuth/button/buttonGoogle.tsx
@@ -1,11 +1,15 @@
 import buttonGoogle from '@/assets/images/btnW_google.png';
+import { GOOGLE_AUTH_URL } from '@/constants/oAuth';
 import Image from 'next/image';
+import Link from 'next/link';
 
 function ButtonGoogle() {
   return (
-    <button className="flex h-[42px] w-[42px] items-center justify-center rounded-full bg-gray-200">
-      <Image src={buttonGoogle} width={30} height={30} alt="google-login" />
-    </button>
+    <Link href={GOOGLE_AUTH_URL}>
+      <button className="flex h-[42px] w-[42px] items-center justify-center rounded-full bg-gray-200">
+        <Image src={buttonGoogle} width={30} height={30} alt="google-login" />
+      </button>
+    </Link>
   );
 }
 
--- src/constants/oAuth.ts
+++ src/constants/oAuth.ts
@@ -7,3 +7,8 @@ export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id
 const NAVER_CLIENT_ID = process.env.NEXT_PUBLIC_NAVER_CLIENT_ID;
 const NAVER_REDIRECT_URI = process.env.NEXT_PUBLIC_NAVER_PROD_REDIRECT_URI;
 export const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&redirect_uri=${NAVER_REDIRECT_URI}`;
+
+// 구글
+const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
+const GOOGLE_REDIRECT_URI = process.env.NEXT_PUBLIC_GOOGLE_PROD_REDIRECT_URI;
+export const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A//www.googleapis.com/auth/drive.metadata.readonly%20https%3A//www.googleapis.com/auth/calendar.readonly&access_type=offline&include_granted_scopes=true&response_type=code&state=state_parameter_passthrough_value&redirect_uri=${GOOGLE_REDIRECT_URI}&client_id=${GOOGLE_CLIENT_ID}`;
