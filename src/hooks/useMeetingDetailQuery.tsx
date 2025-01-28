f85d5ffebc6d07479a5c15e9944d7945a6fd3ff1
kimsayhi <kimse418@gmail.com>
2025-01-29 00:02:52 +0900

    feat: 모임 상세 페이지 SSR 구현

--- src/hooks/useMeetingDetailQuery.tsx
+++ src/hooks/useMeetingDetailQuery.tsx
@@ -1,12 +1,13 @@
 import { fetchMeetingDetail } from '@/api/meeting';
+import { queryKey } from '@/constants/queryKey';
 import { useQuery } from '@tanstack/react-query';
 
 export default function useMeetingDetailQuery(id: number | null) {
-  const queryKey = 'meetingDetail';
-
   return useQuery({
-    queryKey: [queryKey, id],
-    queryFn: () => fetchMeetingDetail(id as number),
+    queryKey: [queryKey.meetingDetail, id],
+    queryFn: () => {
+      return fetchMeetingDetail(id as number);
+    },
     enabled: id !== null,
   });
 }
--- src/pages/meetings/[id].tsx
+++ src/pages/meetings/[id].tsx
@@ -1,13 +1,43 @@
+import { fetchMeetingDetail } from '@/api/meeting';
 import MeetingDetailFooter from '@/components/meetings/MeetingDetailFooter';
 import MeetingDetailMain from '@/components/meetings/MeetingDetailMain';
-import Header from '@/components/shared/layout/Header';
+import { queryKey } from '@/constants/queryKey';
 import useMeetingDetailQuery from '@/hooks/useMeetingDetailQuery';
 import { RootState } from '@/redux/store';
+import {
+  dehydrate,
+  DehydratedState,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
+import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
-export default function MeetingDetailPage() {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { id } = context.query;
+  const queryClient = new QueryClient();
+  const idNum = parseInt(id as string);
+  await queryClient.prefetchQuery({
+    queryKey: [queryKey.meetingDetail, idNum],
+    queryFn: () => fetchMeetingDetail(idNum),
+  });
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+};
+
+interface MeetingDetailPageProps {
+  dehydratedState: DehydratedState;
+}
+
+export default function MeetingDetailPage({
+  dehydratedState,
+}: MeetingDetailPageProps) {
   const [isHost, setIsHost] = useState(false);
   const router = useRouter();
   const { id } = router.query;
@@ -24,12 +54,11 @@ export default function MeetingDetailPage() {
   if (isLoading) return <div>로딩중</div>;
   if (!data || isError) return <div>데이터 불러오기 실패</div>;
   return (
-    <>
-      <Header title="모임 상세" />
+    <HydrationBoundary state={dehydratedState}>
       <div className="mx-auto max-w-screen-md">
         <MeetingDetailMain meetingData={data.data} />
         <MeetingDetailFooter isHost={isHost} />
       </div>
-    </>
+    </HydrationBoundary>
   );
 }
