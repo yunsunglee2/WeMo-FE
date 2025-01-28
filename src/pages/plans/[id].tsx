733c9c119f2416c2622f0355348b07ecdeeff41b
kimsayhi <kimse418@gmail.com>
2025-01-29 00:27:38 +0900

    feat: 일정 상세페이지 SSR 구현

--- src/pages/plans/[id].tsx
+++ src/pages/plans/[id].tsx
@@ -1,12 +1,41 @@
-import { attendPlan, leavePlan } from '@/api/plan';
+import { attendPlan, fetchPlanDetail, leavePlan } from '@/api/plan';
 import PlanDetailMain from '@/components/plans/PlanDetailMain';
-import Header from '@/components/shared/layout/Header';
+import { queryKey } from '@/constants/queryKey';
 import usePlanDetailQuery from '@/hooks/usePlanDetailQuery';
 import { RootState } from '@/redux/store';
+import {
+  dehydrate,
+  DehydratedState,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
+import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
 
-export default function PlanDetailPage() {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { id } = context.query;
+  const queryClient = new QueryClient();
+  const idNum = parseInt(id as string);
+  await queryClient.prefetchQuery({
+    queryKey: queryKey.planDetail(idNum),
+    queryFn: () => fetchPlanDetail(idNum),
+  });
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+};
+
+interface PlanDetailPageProps {
+  dehydratedState: DehydratedState;
+}
+
+export default function PlanDetailPage({
+  dehydratedState,
+}: PlanDetailPageProps) {
   const router = useRouter();
   const { id } = router.query;
   const idNum = parseInt(id as string);
@@ -32,8 +61,7 @@ export default function PlanDetailPage() {
   if (isLoading) return <div>로딩중</div>;
   if (!data) return <div>데이터 없음</div>;
   return (
-    <>
-      <Header title="일정 상세" />
+    <HydrationBoundary state={dehydratedState}>
       <div className="mx-auto min-h-screen max-w-screen-md">
         <PlanDetailMain
           userEmail={auth.user?.email}
@@ -41,6 +69,6 @@ export default function PlanDetailPage() {
           planData={data.data}
         />
       </div>
-    </>
+    </HydrationBoundary>
   );
 }
