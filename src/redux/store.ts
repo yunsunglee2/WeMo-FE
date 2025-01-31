36b03a81610beb44f9ffc7352a9a1284d25082e3
yunsungLEE <chance07236@naver.com>
2025-01-31 15:19:20 +0900

    install: redux devtool

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,23 @@
 import { combineReducers, legacy_createStore as createStore } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import authReducer from './authReducers';
 
 const rootReducer = combineReducers({
   auth: authReducer,
 });
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth'],
+};
+
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
 
-const store = createStore(rootReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer);
 
 export default store;
