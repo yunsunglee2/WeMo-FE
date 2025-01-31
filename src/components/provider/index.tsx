ca7b8f69c5df40d4b7301344012155a792f662c3
yunsungLEE <chance07236@naver.com>
2025-01-31 17:07:27 +0900

    delete: redux persist provider에서 제거

--- src/components/provider/index.tsx
+++ src/components/provider/index.tsx
@@ -4,20 +4,13 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Provider as ReduxProvider } from 'react-redux';
 import store from '@/redux/store';
 import getQueryClient from '@/utils/getQueryClient';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistStore } from 'redux-persist';
-
-// eslint-disable-next-line prefer-const
-export let persistor = persistStore(store);
 
 export default function Provider({ children }: PropsWithChildren) {
   const queryClient = getQueryClient();
   return (
     <ReduxProvider store={store}>
       <QueryClientProvider client={queryClient}>
-        <PersistGate loading={null} persistor={persistor}>
-          {children}
-        </PersistGate>
+        {children}
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </ReduxProvider>
