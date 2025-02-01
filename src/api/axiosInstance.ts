64d01fdb39d7a8b2610b4f9b45a4103fa9ba1ffe
yunsungLEE <chance07236@naver.com>
2025-02-01 11:48:23 +0900

    design: 로그인 리다이렉트 페이지 애니메이션 적용

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 const instance = axios.create({
   baseURL,
-  timeout: 3000,
   headers: {
     'Content-Type': 'application/json',
   },
--- src/pages/login/oauth2/callback/kakao/index.tsx
+++ src/pages/login/oauth2/callback/kakao/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import logoWithColor from '@/assets/images/logo-with-color.png';
 import useOAuthLogin from '@/hooks/useOAuthLogin';
+import { motion } from 'motion/react';
 
 // oAuth 로그인 버튼을 누르면 리다이렉트되는 페이지
 // url에서 autherization code를 받아 서버에 전달한다.
@@ -9,12 +10,18 @@ function KakaoSocialLoginRedirect() {
 
   return (
     <div className="flex h-screen w-full items-center justify-center text-3xl font-extrabold">
-      <Image
-        width={234}
-        height={177}
-        src={logoWithColor}
-        alt="logo-with-color"
-      />
+      <motion.div
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.8, ease: 'easeOut' }}
+      >
+        <Image
+          width={234}
+          height={177}
+          src={logoWithColor}
+          alt="logo-with-color"
+        />
+      </motion.div>
     </div>
   );
 }
