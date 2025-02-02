6001246bcf6dc07aa9d80f3dc5e30a76f2868f44
kimsayhi <kimse418@gmail.com>
2025-02-02 22:42:27 +0900

    refactor: provider컴포  구현 방식 리팩토링

---
+++ src/components/provider/ReduxProvider.tsx
@@ -0,0 +1,7 @@
+import { PropsWithChildren } from 'react';
+import { Provider } from 'react-redux';
+import store from '@/redux/store';
+
+export default function ReduxProvider({ children }: PropsWithChildren) {
+  return <Provider store={store}>{children}</Provider>;
+}
